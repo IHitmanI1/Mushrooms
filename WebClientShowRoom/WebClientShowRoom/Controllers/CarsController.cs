45daa3903074859a609e4c09b5f9a5cc717119c2
IHitmanI1 <TheDENGamer@yandex.ru>
2022-12-26 02:10:29 +0300

    webClient

---
+++ WebClientShowRoom/WebClientShowRoom/Controllers/CarsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebClientShowRoom.Models;
+using System.Linq;
+
+namespace WebClientShowRoom.Controllers
+{
+    public class CarsController : Controller
+    {
+        ApplicationContext db;
+        public CarsController(ApplicationContext context)
+        {
+            db = context;
+        }
+        public ActionResult GetCars()
+        {
+            var prod = db.Cars.ToList();
+            return View(prod);
+        }
+
+    }
+}
---
+++ WebClientShowRoom/WebClientShowRoom/Models/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace WebClientShowRoom.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Cars> Cars { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+
+    }
+}
